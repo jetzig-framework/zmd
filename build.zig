69c97b9dfdbc3ff474fe6eb3f7ce282cb954b914
Bob Farrell <git@bob.frl>
2025-03-01 13:53:27 +0000

    Latest Zig nightly compatibility + disable LLVM

--- build.zig
+++ build.zig
@@ -11,6 +11,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/zmd.zig"),
         .target = target,
         .optimize = optimize,
+        .use_llvm = false,
     });
 
     b.installArtifact(lib);
