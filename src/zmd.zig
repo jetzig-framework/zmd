6c8a751b589207c83791bcc45ae4a03212a79bd0
Ben Jordan <ben@poecoh.com>
2025-08-20 09:06:20 -0400

    putting parse directly in zmd namespace.

--- src/zmd.zig
+++ src/zmd.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
-pub const zmd = @This();
-pub const Zmd = @import("zmd/Zmd.zig");
+const Zmd = @import("zmd/Zmd.zig");
 pub const Node = @import("zmd/Node.zig");
 pub const Ast = @import("zmd/Ast.zig");
 pub const tokens = @import("zmd/tokens.zig");
+pub const parse = Zmd.parse;
 
 test {
     _ = @import("tests.zig");
